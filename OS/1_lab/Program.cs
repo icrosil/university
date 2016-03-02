22319c9380f60074b68355dd2bc1419f669955f3
Illia Olenchenko <icrosil@gmail.com>
2016-03-03 00:02:42 +0200

    Onfly linting  is ON

---
+++ OS/1_lab/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace __lab {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
