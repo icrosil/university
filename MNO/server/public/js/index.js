7f5195657f40047c1501a49a3b3ccda03b4b3d8a
icrosil <icrosil@gmail.com>
2016-05-30 14:53:38 +0300

    Plot with mathjax work

--- MNO/server/public/js/index.js
+++ MNO/server/public/js/index.js
@@ -2,16 +2,28 @@
  * author - Illia Olenchenko
  * group  - PM-1 (OM)
  */
+/* eslint no-param-reassign: 0 */
+/* eslint no-useless-escape: 0 */
 
 // SASS
 require('../scss/index.scss');
 
 // Angular
 const angular = require('angular');
+const _ = require('lodash');
 require('angular-material');
 require('angular-material/angular-material.css');
 
-angular.module('mno', ['ngMaterial']);
+function runConfig($rootScope) {
+  $rootScope.function = 'z = \\sqrt{x^2 + y^2 + 1} + \\frac{x}{2} - \\frac{y}{2}';
+  $rootScope.f = (x, y) => Math.sqrt(x * x + y * y + 1) + x / 2 - y / 2;
+  $rootScope._ = _;
+}
+runConfig.$inject = ['$rootScope'];
+
+angular
+  .module('mno', ['ngMaterial'])
+  .run(runConfig);
 
 // Application
 require('./service/common.js');
--- MNO/server/public/js/plot/plot.js
+++ MNO/server/public/js/plot/plot.js
@@ -4,21 +4,33 @@
  */
 /* eslint no-underscore-dangle: 0 */
 /* eslint no-use-before-define: 0 */
+/* eslint no-undef: 0 */
+/* eslint new-cap: 0 */
+/* eslint no-param-reassign: 0 */
 
 const angular = require('angular');
 const _ = require('lodash');
 const Plotly = require('plotly.js');
+require('mathjax');
+
+MathJax.Hub.Config({
+  skipStartupTypeset: true,
+  messageStyle: 'none',
+  'HTML-CSS': {
+    showMathMenu: false,
+  },
+});
+MathJax.Hub.Configured();
 
 function plotLink($scope) {
+  const vm = this;
+
   $scope.$on('mno.approximations', (e, d) => {
-    _.extend(circles, d);
-    circles.z = _.map(_.zip(d.x, d.y), _.spread(f));
+    _.extend(vm.circles, d);
+    vm.circles.z = _.map(_.zip(d.x, d.y), _.spread($scope.$root.f));
     init();
   });
-  function f(x, y) {
-    return Math.sqrt(x * x + y * y + 1) + x / 2 - y / 2;
-  }
-  const circles = {
+  vm.circles = {
     x: [],
     y: [],
     z: [],
@@ -44,7 +56,7 @@ function plotLink($scope) {
     y.push(i);
     z.push([]);
     for (let j = -100; j < 100; j++) {
-      z[z.length - 1].push(f(j, i));
+      z[z.length - 1].push($scope.$root.f(j, i));
     }
   }
 
@@ -58,7 +70,7 @@ function plotLink($scope) {
   init();
 
   function init() {
-    Plotly.newPlot('plot', [surface, circles]);
+    Plotly.newPlot('plot', [surface, vm.circles]);
   }
 }
 
@@ -69,10 +81,29 @@ function plotDirective() {
   return {
     restrict: 'E',
     templateUrl: '/js/plot/plot.html',
-    link: plotLink,
-    controllerAs: 'plot',
+    controller: plotLink,
+    scope: {},
+    controllerAs: 'plotCtrl',
+  };
+}
+
+function mathjaxBindDirective() {
+  return {
+    restrict: 'A',
+    controller: ['$scope', '$element', '$attrs',
+        function mjbdController($scope, $element, $attrs) {
+          $scope.$watch($attrs.mathjaxBind, (value) => {
+            const $script = angular.element('<script type="math/tex">')
+              .html(value === undefined ? '' : value);
+            $element.html('');
+            $element.append($script);
+            MathJax.Hub.Queue(['Reprocess', MathJax.Hub, $element[0]]);
+          });
+        },
+      ],
   };
 }
 
 angular.module('mno')
-  .directive('mnoPlot', plotDirective);
+  .directive('mnoPlot', plotDirective)
+  .directive('mathjaxBind', mathjaxBindDirective);
