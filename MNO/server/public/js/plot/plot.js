063b7c83147349332c1c7da8b8087617358b2d20
icrosil <icrosil@gmail.com>
2016-06-05 19:04:23 +0300

    Added cache and redraws to plot

--- MNO/server/public/js/plot/plot.js
+++ MNO/server/public/js/plot/plot.js
@@ -22,67 +22,98 @@ MathJax.Hub.Config({
 });
 MathJax.Hub.Configured();
 
-function plotLink($scope, $timeout) {
+function plotLink($scope, $timeout, CacheFactory) {
   const vm = this;
 
+  // Retrieving data
   $scope.$on(`mno.approximations.${vm.id}`, (e, d) => {
     _.extend(vm.circles, d);
     vm.circles.z = _.map(_.zip(d.x, d.y), _.spread($scope.$root.f));
-    init();
+    vm.redraw();
   });
-  vm.query = {
-    limit: 20,
-    page: 1,
-  };
-  vm.circles = {
-    x: [],
-    y: [],
-    z: [],
-    mode: 'line',
-    marker: {
-      color: 'rgb(127, 127, 127)',
-      size: 5,
-      symbol: 'circle',
-      line: {
-        color: 'rgb(204, 204, 204)',
-        width: 1,
-      },
-      opacity: 0.9,
+
+  // Cache instance
+  let plotCache;
+
+  if (!CacheFactory.get(`plot${vm.id}`)) {
+    plotCache = CacheFactory(`plot${vm.id}`);
+  }
+
+  // Settering data
+  const plotDefault = {
+    query: {
+      limit: 20,
+      page: 1,
     },
-    type: 'scatter3d',
-    name: 'Approximations',
+    circles: {
+      x: [],
+      y: [],
+      z: [],
+      mode: 'line',
+      marker: {
+        color: 'rgb(127, 127, 127)',
+        size: 5,
+        symbol: 'circle',
+        line: {
+          color: 'rgb(204, 204, 204)',
+          width: 1,
+        },
+        opacity: 0.9,
+      },
+      type: 'scatter3d',
+      name: 'Approximations',
+    },
+    sizes: {
+      from: -50,
+      to: 50,
+      get times() {
+        return Math.abs(this.from) + Math.abs(this.to);
+      },
+    },
+    surface: {
+      z: [],
+      x: [],
+      y: [],
+      type: 'surface',
+      name: 'Real function',
+    },
   };
-  const z = [];
-  const x = [];
-  const y = [];
 
-  _.times(100, (ival) => {
-    const i = ival - 50;
-    x.push(i);
-    y.push(i);
-    z.push([]);
-    _.times(100, (jval) => {
-      const j = jval - 50;
-      z[z.length - 1].push($scope.$root.f(j, i));
+  vm.plotParams = _.defaults(plotCache.get('/options') || {}, plotDefault);
+
+  function setPoints() {
+    const x = vm.plotParams.surface.x;
+    const y = vm.plotParams.surface.y;
+    const z = vm.plotParams.surface.z;
+    x.length = 0;
+    y.length = 0;
+    z.length = 0;
+    _.times(vm.plotParams.sizes.times, (ival) => {
+      const i = ival - vm.plotParams.sizes.times / 2;
+      x.push(i);
+      y.push(i);
+      z.push([]);
+      _.times(vm.plotParams.sizes.times, (jval) => {
+        const j = jval - vm.plotParams.sizes.times / 2;
+        z[z.length - 1].push($scope.$root.f(j, i));
+      });
     });
-  });
-
-  const surface = {
-    z,
-    x,
-    y,
-    type: 'surface',
-    name: 'Real function',
+  }
+  vm.redraw = () => {
+    plotCache.put('/options', angular.copy(vm.plotParams));
+    setPoints();
+    Plotly.redraw(vm.id);
   };
-
-  init();
 
   function init() {
-    $timeout(_.partial(Plotly.newPlot, vm.id, [surface, vm.circles]));
+    setPoints();
+    $timeout(_.partial(Plotly.newPlot, vm.id, [vm.plotParams.surface, vm.plotParams.circles]));
   }
+
+  init();
 }
 
-plotLink.$inject = ['$scope', '$timeout'];
+plotLink.$inject = ['$scope', '$timeout', 'CacheFactory'];
 
 // Directives
 function plotDirective() {
