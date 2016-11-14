3189747b7e7025ce0a1c621bd738bb4721c14a1e
icrosil <icrosil@gmail.com>
2016-11-14 22:34:58 +0200

    added e to formulas

--- PBA/lab2/main.py
+++ PBA/lab2/main.py
@@ -4,6 +4,8 @@ authors: Illia Olenchenko, Maxim Manzuk.
 from scipy.integrate import odeint
 import numpy as np
 import math
+from plotHelper import drawSolution
+from helpers import convertArrayToMatrix, convertMatrixToArray
 
 from functions.r import R
 from functions.p import P
@@ -48,7 +50,7 @@ def main():
   # w - vector with y
   w = lambda t: [1, 1, 1]
   # ea - external power
-  ea = lambda t: 1. / (t + 1)
+  ea = lambda t: t ** 2 / (t + 1)
   # A - main matrix of diff system
   A = lambda t: [
     [0, 1, 0],
@@ -57,15 +59,15 @@ def main():
   ]
   # N - one of additional predefined matrices
   N = lambda t: [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
+    [2, 0, 1],
+    [0, 2, 0],
+    [1, 0, 2]
   ]
   # M - one of additional predefined matrices
   M = lambda t: [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
+    [2, 0, 1],
+    [0, 2, 0],
+    [1, 0, 2]
   ]
   # C - second part for main diff
   C = lambda t: [ 0, 0, ea(t) / La ]
@@ -83,9 +85,9 @@ def main():
 
   # P0 - coeff
   P0 = np.array([
-    [1, 2, 3],
-    [5, 1, 4],
-    [6, 1, 1]
+    [2, 0, 1],
+    [0, 2, 0],
+    [1, 0, 2]
   ])
   # coeff for P0 ** -1
   P0_1 = np.linalg.inv(P0)
@@ -93,9 +95,20 @@ def main():
   k0 = 0
   # coeff for a
   a = [0, 0, 0]
+  # coeff for mu, should be binded with other functions
+  mu = 7
 
   rt = R(timeStart, timeEnd, P0_1, A, C, G, N, M, times)
   xt = X(timeStart, timeEnd, A, C, G, N, M, y, w, P0_1, a, rt, times)
   kt = K(timeStart, timeEnd, A, G, N, y, w, xt, times)
+  # decomprehension
+  kt = [kti[0] for kti in  kt]
+  rSmallT = [math.sqrt((mu ** 2) - kti) for kti in kt]
+  rSmallTSquared = [rti ** 2 for rti in rSmallT]
+  shapeEllipseMatrix = [convertArrayToMatrix(np.dot(rSmallTSquared[i], rt[i])) for i in range(timeCount)]
+  et = [max(np.linalg.eig(shape)[0]) for shape in shapeEllipseMatrix]
+  etNorm = [rSmallT[i] * math.sqrt(et[i]) for i in range(timeCount)]
+  print etNorm
+  # drawSolution(times, xt, shapeEllipseMatrix, [0, 1])
 
 main()
